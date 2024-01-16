98f91caa521ba8385d2242d1931c7847a3336248
Shammon Shamsudeen <shamon@shamon.com>
2024-01-16 14:53:21 +0530

    typo fixes

--- utils.sh
+++ utils.sh
@@ -41,7 +41,7 @@ if [[ $selections[login] == 'y' ]]; then
     echo "Logging to ArgoCD CLI....."
     echo 
     export ARGOCD_USERNAME=admin
-    export ARGOCD_PASSWORD=gD2Wdy6iYxDDeWki
+    export ARGOCD_PASSWORD=wRvTDHAOJV1pNwU6
     export ARGOCD_SERVER=localhost:8080
     argocd login $ARGOCD_SERVER --username $ARGOCD_USERNAME --password $ARGOCD_PASSWORD
 fi
